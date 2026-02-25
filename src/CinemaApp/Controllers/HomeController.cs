cdc5006a2bad4bb5967a53e760d0fcfaf9bc47ea
dimityr Petkov <hi6tnikyt@abv.bg>
2026-02-25 19:49:58 +0200

    Add appsettings.Development.json

---
+++ src/CinemaApp/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+namespace CinemaApp.Web.Controllers
+{
+    using System.Diagnostics;
+    using CinemaApp.Web.ViewModels;
+    using Microsoft.AspNetCore.Mvc;
+    public class HomeController : Controller
+    {
+
+        public HomeController()
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
