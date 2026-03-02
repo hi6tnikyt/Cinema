d0305a4b0d78eb1a66a0c3f9fed42d0c3986268c
dimityr Petkov <hi6tnikyt@abv.bg>
2026-03-02 20:25:36 +0200

    Add Service  layer

---
+++ src/CinemaApp/Controllers/MovieController.cs
@@ -0,0 +1,15 @@
+﻿using CinemaApp.Web.ViewModels.Movie;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CinemaApp.Web.Controllers
+{
+    public class MovieController : BaseController
+    {
+        [AllowAnonymous]
+        public IActionResult Index()
+        {
+            return View(new List<AllMoviesIndexViewModel>());
+        }
+    }
+}
