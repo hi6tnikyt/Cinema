be3ca7ff4b2e329ffd7efde2526e99012a3b9f90
dimityr Petkov <hi6tnikyt@abv.bg>
2026-03-04 23:57:57 +0200

    Add Create button

---
+++ src/CinemaApp.Data/Repository/MovieRepository.cs
@@ -0,0 +1,32 @@
+﻿
+using CinemaApp.Data.Models;
+using CinemaApp.Data.Repository.Contracts;
+using Microsoft.EntityFrameworkCore;
+
+namespace CinemaApp.Data.Repository
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly CinemaAppDbContext dbContext;
+        public MovieRepository(CinemaAppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Movie>> GetAllMovies()
+        {
+            return await this.dbContext
+                 .Movies
+                 .AsNoTracking()
+                 .OrderBy(m => m.Title)
+                 .ToArrayAsync();
+        }
+
+        public IQueryable<Movie> GetAllMoviesNoTracking()
+        {
+            return this.dbContext
+                 .Movies
+                 .AsNoTracking();
+        }
+    }
+}
---
+++ src/CinemaApp.Web.ViewModels/Movie/MovieFormModel.cs
@@ -0,0 +1,20 @@
+﻿
+namespace CinemaApp.Web.ViewModels.Movie
+{
+    public class MovieFormModel
+    {
+        public string Title { get; set; } = null!;
+
+        public string Genre { get; set; } = null!;
+
+        public string Director { get; set; } = null!;
+
+        public int Duration { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Description { get; set; } = null!;
+
+        public string? ImageUrl { get; set; }
+    }
+}
